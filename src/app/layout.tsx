ae987b895a9b446a8d31cbec58e1f820b6c03113
siga10 <wallpapersk912@gmail.com>
2025-09-05 05:03:26 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,34 +19,11 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "ART HOME | Luxury Interior & Exterior Finishing",
-  description:
-    "ART HOME - Luxury interior and exterior finishing: execution, design, supervision and high quality.",
+  description: "Luxury interior and exterior finishing: execution, design, supervision and high quality.",
   icons: {
-    icon: "/logo.png", // لازم تكون الصورة موجودة بـ public/logo.png
-  },
-  openGraph: {
-    title: "ART HOME | Luxury Interior & Exterior Finishing",
-    description: `ART HOME - Luxury interior and exterior finishing: execution, design, supervision and high quality.`,
-    url: "https://www.arthomeco.com/", // غيّر للرابط الرسمي تبعك
-    siteName: "ART HOME",
-    images: [
-      {
-        url: "/logo.png", // أو استبدلها بصورة preview كبيرة (1200x630px)
-        width: 1200,
-        height: 630,
-        alt: "ART HOME Logo",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "ART HOME | Luxury Interior & Exterior Finishing",
-    description: `ART HOME - Luxury interior and exterior finishing: execution, design, supervision and high quality.`,
-    images: ["/logo.png"],
+     icon: "/logo.png", // ضيف الأيقونة هنا
   },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
